91bdc21a29961a77a319b914ebb251b0ba18e08d
NiravRathva <niravrathwa57@gmail.com>
2023-08-16 19:41:19 +0530

    added Post component

---
+++ src/Components/Post.js
@@ -0,0 +1,54 @@
+import React from "react";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import SendIcon from "@mui/icons-material/Send";
+import CommentIcon from "@mui/icons-material/Comment";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+
+const Post = () => {
+  return (
+    <div className=" rounded-md shadow-md p-8 max-w-md mb-2 mx-auto border-b border-gray-700">
+      <div className="flex items-center justify-between mb-2">
+        {/* userName and profile */}
+        <div className="flex items-center">
+          <AccountCircleIcon className="text-blue-500 text-xl" />
+          <h5 className="ml-2 font-semibold">_careless_dude_</h5>
+        </div>
+        {/* more Icon */}
+        <div>
+          <MoreHorizIcon className="text-gray-500" />
+        </div>
+      </div>
+      {/* photo or Video */}
+      <div>
+        <img
+          src="https://assets.architecturaldigest.in/photos/64cc0230b462697ebd0b5f2b/master/w_1600,c_limit/DSC00560-2.jpg"
+          alt=""
+          className="w-96 rounded-lg shadow-md"
+        />
+      </div>
+
+      <div className="mt-4 flex justify-between ">
+        {/* likes, comment and share */}
+        <div className="flex space-x-4 ">
+          <FavoriteBorderIcon />
+          <CommentIcon />
+          <SendIcon />
+        </div>
+        {/* save Icon */}
+        <div className="">
+          <BookmarkBorderIcon />
+        </div>
+      </div>
+
+      <div>
+        <h6 className="my-1">1001 likes</h6>
+        <h6 className="my-1"><span className="font-semibold">_careless_dude_</span> loves to work</h6>
+        <input type="text" placeholder="Add a comment...." className="bg-inherit border-none outline-none" />
+      </div>
+    </div>
+  );
+};
+
+export default Post;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,20 +1,31 @@
 import React from "react";
+import Post from "../Components/Post";
 import Sidebar from "../Components/Sidebar";
 
 const Home = () => {
   return (
     <div className="flex">
       {/* Sidebar */}
-      <div className="w-1/6 h-screen bg-black text-white ">
+      <div className="w-1/6 h-screen bg-black text-white border-r border-gray-700 ">
         <Sidebar />
       </div>
 
       {/* Main Content */}
-      <div className=" flex w-5/6  p-4">
+      <div className="flex  flex-1 ml-1/6 w-5/6  p-4 bg-black text-white overflow-y-auto">
         {/* posts */}
-        <div className="w-3/4">post</div>
+        <div className="w-3/4 bg-black text-white">
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+        </div>
         {/* suggestion */}
-        <div className="w-1/4">Suggestions</div>
+        <div className="w-1/4 bg-red-200">Suggestions</div>
       </div>
     </div>
   );
